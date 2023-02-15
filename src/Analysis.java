d00da921d550e32f2c17b98cab835b0f9c9f6749
qsebso <quinnsebso@gmail.com>
2023-02-15 14:26:37 -0700

    2/15/23 2:23pm

--- src/Analysis.java
+++ src/Analysis.java
@@ -3,13 +3,13 @@ import java.util.Map;
 public class Analysis {
 
     public Analysis() {
-        Data data = new Data();
+        //
     }
 
     // Given a course ID give back some sort of information
     public String courseInfo() {
         String coursePoints = "";
-        for (Map.Entry<Integer,Integer> entry : data.getCoursePointsData.entrySet()) {
+        for (Map.Entry<Integer,Integer> entry : Data.getCoursePointsData.entrySet()) {
             coursePoints = "Number of points used: " + entry.getKey() + ", Number of people = " + entry.getValue();
         }
         return coursePoints;
