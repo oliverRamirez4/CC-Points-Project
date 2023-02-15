8230d9dd39d8b272a9c99640c9bb638213b6df2f
qsebso <quinnsebso@gmail.com>
2023-02-15 10:43:36 -0700

    2/15/23 10:43am

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.HashMap;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+//        HashMap<String, Integer> map = null;
+//
+//        // Deserialize the HashMap
+//        try (FileInputStream fileIn = new FileInputStream("Data.ser");
+//             ObjectInputStream in = new ObjectInputStream(fileIn)) {
+//            map = (HashMap<String, Integer>) in.readObject();
+//        } catch (IOException | ClassNotFoundException e) {
+//            e.printStackTrace();
+//        }
+
     }
 }
