422779c784c9916afa0436302b3cdfd74e596975
ddentona <63802540+ddentona@users.noreply.github.com>
2023-02-16 10:00:04 -0700

    2023.02.16 10:00 MST

--- src/Data.java
+++ src/Data.java
@@ -21,11 +21,19 @@ public class Data {
     private Map<String, Map<String, Map<Integer, Integer>>> coursePointsData;
     private Map<Integer, List<String>> data;
 
+    /**
+     * Level 1: Course ID
+     * Level 2: Block Number
+     * Level 3: (1) minimum points, (2) number of seats, (3) demand
+     */
+    private Map<String, Map<String, List<Integer>>> minMaxPoints;
+
     public XSSFWorkbook workbook;
 
     public Data(String fileLocation) throws IOException, InvalidFormatException {
         data = new HashMap<>();
         coursePointsData = new HashMap<>();
+        minMaxPoints = new HashMap<>();
         workbook = new XSSFWorkbook(new File(fileLocation));
     }
 
@@ -92,6 +100,71 @@ public class Data {
         }
     }
 
+    // Adds the course minimum, maximum, etc. points to the Data file
+    public void addToMinMaxPointsData(int fileNumber) throws IOException, InvalidFormatException {
+        //Data assurance so I don't put something into nothing
+        String name = getCourseID(fileNumber);
+        String block = getCourseBlock(fileNumber);
+        minMaxPoints.put(name, new HashMap<>());
+        minMaxPoints.get(name).put(block, new ArrayList<>());
+
+        Sheet sheet = workbook.getSheetAt(fileNumber);
+
+        //Add the minimum points
+        int i;
+        try {
+            i = (int) sheet.getRow(4).getCell(7).getNumericCellValue();
+        } catch (NullPointerException n) {
+            i = 0;
+        }
+        minMaxPoints.get(name).get(block).add(0, i);
+
+        //Add the number of seats
+        try {
+            i = Integer.valueOf(charsBtwn(sheet.getRow(1).getCell(6).getStringCellValue(), 7, 8));
+        } catch (NullPointerException n) {
+            i = 0;
+        }
+        minMaxPoints.get(name).get(block).add(1, i);
+
+        //Add the seat demand
+        try {
+            i = Integer.valueOf(charsBtwn(sheet.getRow(2).getCell(6).getStringCellValue(), 8, 9));
+        } catch (NullPointerException n) {
+            i = 0;
+        }
+        minMaxPoints.get(name).get(block).add(2, i);
+    }
+
+    public Map<String, Map<String, List<Integer>>> getMinMaxPoints() {
+        return minMaxPoints;
+    }
+
+    public String getPrintableMinMaxPointsData() {
+        String printData = "";
+        for (String course : minMaxPoints.keySet()) {
+            printData += ("Course " + course + "\n");
+            for (String block : minMaxPoints.get(course).keySet()) {
+                printData += block;
+                printData += "\t";
+
+                printData += "Minimum points: ";
+                printData += minMaxPoints.get(course).get(block).get(0);
+                printData += "\t";
+
+                printData += "Seat total: ";
+                printData += minMaxPoints.get(course).get(block).get(1);
+                printData += "\t";
+
+                printData += "Seat demand: ";
+                printData += minMaxPoints.get(course).get(block).get(2);
+                printData += "\t";
+            }
+            printData += "\n";
+        }
+        return printData;
+    }
+
     public String getPrintData() {
         String printData = "";
         for (Integer key : data.keySet()) {
@@ -139,6 +212,14 @@ public class Data {
         return id;
     }
 
+    private String charsBtwn(String input, int start, int end) {
+        String id = "";
+        try {
+            for (int i = start; i <= end; i++) id += input.toCharArray()[i];
+        } catch (ArrayIndexOutOfBoundsException a) {;}
+        return id;
+    }
+
     public Map<Integer, List<String>> getData() {
         return data;
     }
@@ -147,6 +228,26 @@ public class Data {
     public static void main(String[] args) throws IOException {
         try {
             String url = "./src/CP222 - Projet/2020-11-19 - Course Demand and Point Distribution.xlsx", courseID, courseBlock; int block;
+
+            Data dataMain = new Data(url);
+            for (int i = 0; i < dataMain.workbook.getNumberOfSheets(); i++) {
+                courseID = dataMain.getCourseID(i);
+                courseBlock = dataMain.getCourseBlock(i);
+                //FileOutputStream fileOut = new FileOutputStream("src/usableData/2021S/" + courseID + courseBlock + ".ser");
+                //ObjectOutputStream out = new ObjectOutputStream(fileOut);
+                try {
+                    dataMain.addToMinMaxPointsData(i);
+                } catch (InvalidFormatException e){
+                    throw new RuntimeException(e);
+                }
+                //out.writeObject(dataMain.getCoursePointsData());
+                //out.close();
+                //fileOut.close();
+
+                System.out.println(dataMain.getPrintableMinMaxPointsData());
+
+            }
+
             /*Data analysis = new Data(url);
             for (int i = 0; i < analysis.workbook.getNumberOfSheets(); i++) {
                 courseID = analysis.getCourseID(i);
@@ -157,7 +258,7 @@ public class Data {
                 //System.out.println(analysis.getCoursePointsData());
                 System.out.println();
             }*/
-            Data dataMain = new Data(url);
+            /*Data dataMain = new Data(url);
 
             for (int i = 0; i < dataMain.workbook.getNumberOfSheets(); i++) {
                 courseID = dataMain.getCourseID(i);
@@ -175,7 +276,7 @@ public class Data {
 
                 System.out.println("object info saved");
 
-            }
+            }*/
             //analysis.readJExcel("./src/CP222 - Projet/2020-11-19 - Course Demand and Point Distribution.xlsx", 0);
             //System.out.println(analysis.getPrintData());
         } catch (IOException | InvalidFormatException e) {
