720cbca4a9057dd1a633e1664d6f720f0acf844c
yaelhoma <yrhoma@gmail.con>
2023-02-16 10:54:48 -0700

    2/16/23 10:54am

--- src/Window.java
+++ src/Window.java
@@ -1,3 +1,5 @@
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
@@ -5,8 +7,12 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Set;
 
 public class Window {
+    Data data;
     JFrame topFrame;
     JPanel startPanel;
     Course coursePanel;
@@ -15,7 +21,9 @@ public class Window {
     JComboBox classSelector;
     JButton goButton;
 
-    public Window() {
+    String [] courses;
+
+    public Window() throws IOException, InvalidFormatException {
 
         //create the JFrame
         topFrame = new JFrame();
@@ -28,13 +36,30 @@ public class Window {
 
         //create a JLabel containing the CC Logo
         CCLogo = new JLabel(new ImageIcon("src/CC-Logo-Stacked.png"));
+        String url = "./src/CP222 - Projet/2020-11-19 - Course Demand and Point Distribution.xlsx", courseID, courseBlock; int block;
+        data = new Data(url);
+        for (int i = 0; i < data.workbook.getNumberOfSheets(); i++) {
+            courseID = data.getCourseID(i);
+            courseBlock = data.getCourseBlock(i);
+            //FileOutputStream fileOut = new FileOutputStream("src/usableData/2021S/" + courseID + courseBlock + ".ser");
+            //ObjectOutputStream out = new ObjectOutputStream(fileOut);
+            try {
+                data.addToMinMaxPointsData(i);
+            } catch (InvalidFormatException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        courses = data.getCourseList(data);
+        classSelector = new JComboBox(courses);
+
 
         //this combo box will have a drop down menu of all of the classes
-        classList = new String[]{"choose your course","cp122","ap123","dp124"};//place filler until we have all the classes
-        classSelector = new JComboBox(classList);
+
 
         //go Button to request info of course
         goButton = new JButton("go");
+
+
     }
 
     void Display(){
@@ -54,6 +79,7 @@ public class Window {
         startPanel.add(classSelector);
 
         startPanel.add(goButton);
+        System.out.print(Arrays.toString(courses));
         goButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -66,7 +92,8 @@ public class Window {
         topFrame.setVisible(true);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InvalidFormatException {
+
         Window w = new Window();
         w.Display();
     }
