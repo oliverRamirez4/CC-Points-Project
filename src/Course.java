d7cfac4cc1003e35e04bb56e2ab5decf4be52e61
ddentona <63802540+ddentona@users.noreply.github.com>
2023-02-16 12:19:50 -0700

    2023.02.16 12:19 MST

--- src/Course.java
+++ src/Course.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.util.HashMap;
 
 //this class might be unnecessary
 // don't need to work on it
@@ -6,17 +7,27 @@ public class Course extends JPanel {
 
     public String courseID, blockNumber;
     public int minPoints;
+    public HashMap<Integer, Integer> pointBids;
+
+    public Course(String courseID, String blockNumber, int minPoints, HashMap<Integer, Integer> pointBids) {
+        this.courseID = courseID;
+        this.blockNumber = blockNumber;
+        this.minPoints = minPoints;
+        this.pointBids = pointBids;
+    }
 
     public Course(String courseID, String blockNumber, int minPoints) {
         this.courseID = courseID;
         this.blockNumber = blockNumber;
         this.minPoints = minPoints;
+        this.pointBids = null;
     }
 
     public Course() {
         this.courseID = "";
         this.blockNumber = "";
         this.minPoints = -1;
+        pointBids = null;
     }
 
     public String getCourseID(){
@@ -31,5 +42,8 @@ public class Course extends JPanel {
         return blockNumber;
     }
 
+    public HashMap<Integer, Integer> getPointBids() {
+        return pointBids;
+    }
 
 }
