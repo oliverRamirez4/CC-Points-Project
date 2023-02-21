a076b562451fd38d90b220d87a3f94a6fe2a9743
yaelhoma <yrhoma@gmail.con>
2023-02-21 15:26:36 -0700

    2/21/2023 3:25pm MST

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Window w = new Window();
+        w.display();
+        Data data = new Data();
+        System.out.println(data.getAllData().get("2023S").get("SP101").keySet());
     }
 }
